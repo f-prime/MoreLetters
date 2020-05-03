22d641c8a9fc6256beecf1a82b4775bd55dee9ff
Frankie Primerano <frankieprimerano@gmail.com>
2020-05-03 19:34:44 -0400

    Removed zero checks

--- js/tick.js
+++ js/tick.js
@@ -10,41 +10,26 @@ export function updateLetters() {
 }
 
 export function updateCorporateOffices() {
-  if(this.corporateOffices <= 0)
-    return;
-
   const delta = this.generalUpdate(this.corporateOfficesDelay);
   this.corporateOfficeAdded += this.getCorporateOfficesIncrease * this.corporateOffices * delta;
 }
 
 export function updateAdvertisers() {
-  if(this.advertisers <= 0)
-    return;
-
   const delta = this.generalUpdate(this.advertisersDelay);
   this.clickInc += this.advertisersInc * this.advertisers * delta;
 }
 
 export function updateBreeder() {
-  if(this.breeders <= 0)
-    return;
-
   const delta = this.generalUpdate(this.breederDelay);
   this.pigeons += this.breederBreed * this.breeders * delta;
 }
 
 export function updatePigeons() {
-  if(this.pigeons <= 0)
-    return;
-
   const delta = this.generalUpdate(this.getPigeonsDelay);
   this.deliverLetter(this.getPigeonsDelivery * this.pigeons * delta);
 }
 
 export function updateMailboxes() {
-  if(this.mailboxes <= 0)
-    return;
-
   const delta = this.generalUpdate(this.getMailboxDelay);
   const inc = (this.mailboxes + this.factoryMailboxes) * this.getMailboxLettersInc * delta;
   this.letters += inc;
@@ -60,9 +45,6 @@ export function updateMailmen() {
 }
 
 export function updateFactories() {
-  if(this.factories <= 0)
-    return;
-
   const delta = this.generalUpdate(this.getFactoryDelay);
   this.factoryMailboxes += this.factoryGenerate * (this.factories + this.corporateOfficeAdded) * delta;
 
